60eb001b7fdc2a9267972f6d7cdc4c6e5431400e
gustavoandrade18 <gustavoandraderoom18@gmail.com>
2025-06-20 19:21:24 -0300

    Adicionei mais projetos e melhorei algumas coisas

--- scripts.js
+++ scripts.js
@@ -3,13 +3,26 @@ const detalhes = document.querySelectorAll(".detalhes-texto");
 
 detalhes.forEach(p => {
   p.addEventListener("click", (event) => {
-    // Para evitar que o clique suba para o card, evita propagação
-    event.stopPropagation();
 
-    // O pai do p é o card, então toggle nele
+    // O pai do p é o card, então muda ele
     const card = p.closest(".projeto-card");
     if (card) {
       card.classList.toggle("ativo");
     }
   });
 });
+  // Zoom de imagens
+  const overlay = document.getElementById('imageOverlay');
+  const zoomedImage = document.getElementById('zoomedImage');
+  const zoomables = document.querySelectorAll('.zoomable');
+
+  zoomables.forEach(img => {
+    img.addEventListener('click', () => {
+      zoomedImage.src = img.src;
+      overlay.style.display = 'flex';
+    });
+  });
+
+  overlay.addEventListener('click', () => {
+    overlay.style.display = 'none';
+  });
